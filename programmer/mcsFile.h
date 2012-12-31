4e7ac50ad844d62b048240e574b4845738fd5bdf
Peter Kazanzides <pkaz@jhu.edu>
2012-12-31 04:25:20 +0000

    Added software/programmer.
    git-svn-id: https://svn.lcsr.jhu.edu/mechatronics/trunk/Software@32 95aa9265-a27f-408e-9981-efeafd1b3a66

---
+++ programmer/mcsFile.h
@@ -0,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+#ifndef _MCS_H
+#define _MCS_H
+
+// mcsFile
+//
+// This class reads an Intel MCS-86 format file, which is produced by the Xilinx
+// ISE software. It is not a complete implementation, and probably will not work
+// for MCS-86 files produced by other software packages.
+
+#include <string>
+#include <fstream>
+
+class mcsFile {
+    std::ifstream file;
+    int line_num;                    // current line number
+    unsigned long startAddr;         // start address
+    unsigned char curSector[65536];  // current sector
+
+    struct RecInfo {
+        enum { DATA_MAX = 16 };
+        unsigned char ndata;
+        unsigned short addr;
+        unsigned char type;
+        unsigned char data[DATA_MAX];
+    };
+
+    bool ProcessNextLine(RecInfo &rec);
+    bool toHex(const char *p2, unsigned char &result) const;
+
+    enum RecordTypes {
+        RECORD_DATA = 0,
+        RECORD_EOF = 1,
+        RECORD_EXT_SEGMENT = 2,  // not supported here
+        RECORD_EXT_LINEAR = 4
+    };
+
+public:
+    mcsFile();
+    ~mcsFile();
+    bool OpenFile(const std::string &fileName);
+    bool ReadNextSector();
+    unsigned long GetSectorAddress() const { return startAddr; }
+    const unsigned char *GetSectorData() const { return curSector; }
+    void CloseFile();
+};
+
+#endif // _MCS_H
