a38c0b06224af65da79fed402e958632391d8fd3
Anton Deguet <anton.deguet@jhu.edu>
2020-12-16 16:38:48 -0500

    Added PortFactory function, updated qladisp and qlacloserelays to use PortFactory

---
+++ lib/code/PortFactory.cpp
@@ -0,0 +1,74 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s):  Anton Deguet
+
+  (C) Copyright 2014-2020 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+#include "PortFactory.h"
+
+#include <Amp1394/AmpIORevision.h>
+#if Amp1394_HAS_RAW1394
+#include "FirewirePort.h"
+#endif
+#if Amp1394_HAS_PCAP
+#include "EthRawPort.h"
+#endif
+#include "EthUdpPort.h"
+
+BasePort * PortFactory(const char * args, std::ostream & debugStream)
+{
+    BasePort * port = 0;
+    int portNumber = 0;
+    std::string IPaddr = ETH_UDP_DEFAULT_IP;
+
+#if Amp1394_HAS_RAW1394
+    BasePort::PortType portType = BasePort::PORT_FIREWIRE;
+#else
+    BasePort::PortType portType = BasePort::PORT_ETH_UDP;
+#endif
+
+    if (!BasePort::ParseOptions(args, portType, portNumber, IPaddr)) {
+        debugStream << "PortFactory: Failed to parse option: " << args << std::endl;
+        return port;
+    }
+
+    switch (portType) {
+
+    case BasePort::PORT_FIREWIRE:
+#if Amp1394_HAS_RAW1394
+        port = new FirewirePort(portNumber, debugStream);
+#else
+        debugStream << "PortFactory: FireWire not available (set Amp1394_HAS_RAW1394 in CMake)" << std::endl;
+#endif
+        break;
+
+    case BasePort::PORT_ETH_UDP:
+        port = new EthUdpPort(portNumber, IPaddr, debugStream);
+        break;
+    
+    case BasePort::PORT_ETH_RAW:
+#if Amp1394_HAS_PCAP
+        port = new EthRawPort(portNumber, debugStream);
+#else
+        debugStream << "PortFactory: Raw Ethernet not available (set Amp1394_HAS_PCAP in CMake)" << std::endl;
+#endif
+        break;
+
+    default:
+        debugStream << "PortFactory: Unsupported port type" << std::endl;
+        break;
+    }
+    
+    return port;
+}
--- tests/qlacloserelays.cpp
+++ tests/qlacloserelays.cpp
@@ -19,51 +19,53 @@ http://www.cisst.org/cisst/license.txt.
 #include <stdlib.h> // for atoi
 #include <unistd.h> // for usleep
 #include <iostream>
+#include <vector>
 
-#include <FirewirePort.h>
+#include <PortFactory.h>
 #include <AmpIO.h>
 
 int main(int argc, char** argv)
 {
     unsigned int i, j;
-    int port = 0;
+    BasePort * port = 0;
+    std::string portArgs;
     bool closing = true;
 
     for (i = 1; i < (unsigned int)argc; i++) {
         if ((argv[i][0] == '-') && (argv[i][1] == 'p')) {
-            port = atoi(argv[i]+2);
+            portArgs = argv[i]+2;
         }
         if ((argv[i][0] == '-') && (argv[i][1] == 'o')) {
             closing = false;
         }
     }
 
-    std::cout << "Using firewire port " << port << " (to change port, use -p)" << std::endl;
+    // if port is not specified
+    port = PortFactory(portArgs.c_str());
+    if (!port) {
+        std::cerr << "Failed to create port using: " << portArgs << std::endl;
+        return -1;
+    }
+
     if (closing) {
         std::cout << "Closing all relays (to open all, use -o)" << std::endl;
     } else {
         std::cout << "Opening all relays" << std::endl;
     }
 
-    FirewirePort Port(port, std::cout);
-    if (!Port.IsOK()) {
-        std::cerr << "Failed to initialize firewire port " << port << std::endl;
-        return -1;
-    }
-
-    std::vector<AmpIO *> BoardList;
-    for (i = 0; i < Port.GetNumOfNodes(); i++) {
-        int boardId = Port.GetBoardId(i);
+    std::vector<AmpIO *> boardList;
+    for (i = 0; i < port->GetNumOfNodes(); i++) {
+        int boardId = port->GetBoardId(i);
         if (boardId != BoardIO::MAX_BOARDS) {
             std::cout << "Adding firewire Node " << i << ", BoardID " << boardId << std::endl;
             AmpIO *board = new AmpIO(boardId);
-            BoardList.push_back(board);
-            Port.AddBoard(board);
+            boardList.push_back(board);
+            port->AddBoard(board);
         }
     }
 
-    for (j = 0; j < BoardList.size(); j++) {
-        BoardList[j]->WriteSafetyRelay(closing);
+    for (j = 0; j < boardList.size(); j++) {
+        boardList[j]->WriteSafetyRelay(closing);
         usleep(40000); // sleep 40 ms
     }
 
@@ -72,5 +74,8 @@ int main(int argc, char** argv)
     } else {
         std::cout << "Safety relays opened" << std::endl;
     }
+
+    delete port;
+
     return 0;
 }
