df5cd9335bf38dc0e76470ed0503a36b1d6f7b93
Anton Deguet <anton.deguet@jhu.edu>
2021-01-25 12:04:19 -0500

    pgm1394multi.bash: added check in loop to interrupt if pgm1394 fails on last board

--- programmer/pgm1394multi.bash
+++ programmer/pgm1394multi.bash
@@ -1,6 +1,23 @@
 #!/bin/bash
-for boardId in "$@"
+#
+# Script to run pgm1394 on multiple boards.  This script doesn't reboot the boards.
+# Usage: pgm1394multi 0 1 6 7 # parameters are board IDs
+#
+
+boards="$@"
+
+for boardId in $boards
 do
     echo "Programming board $boardId"
-    pgm1394 $boardId -a 
+    pgm1394 $boardId -a
+    result=$?
+    echo "Result: $result (0 is good)"
+    if [[ "$result" -ne 0 ]]; then
+       echo "------> pgm1394 -a failed for board $boardId"
+       echo "------> DO NOT REBOOT OR POWER OFF this board"
+       echo "------> Try to reprogram this board unsing pgm1394 with pgm1394"
+       exit $result
+    fi
 done
+
+exit 0
